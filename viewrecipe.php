38cc569e259be819ffe11280731cdc92892ca8fe
Philip <philipbrooksallen@gmail.com>
2021-03-27 21:30:12 -0400

    initial commit

---
+++ viewrecipe.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'core/init.php';
+
+/*if(Session::exists('home')) {
+    echo '<p>' . Session::flash('home'). '</p>';
+}*/
+
+$user = new User(); //Current
+
+if($user->isLoggedIn()) {
+    if (Input::exists('get')) {
+        $recipeID = Input::get('recipeid');
+        if ($recipeID) {
+?>
+    <p><a href="index.php">"Back to list"</a></p>
+    <p>recipe for <?php echo($recipeID) ?> goes here</p>
+        
+<?php            
+        }
+    }
+
+} else {
+    echo '<p>You need to <a href="login.php">login</a> or <a href="register.php">register.</a></p>';
+}
